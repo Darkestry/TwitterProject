022501839add3245ca49582c0d1655e7dcde84a3
Darkestry <73218288+Darkestry@users.noreply.github.com>
2021-11-10 18:16:41 +0100

    Data cleansing #2

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import json
+import regex as re
+from time import sleep
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
+consumer_key = "iOWKyxGh5PrZwGR6w99Y1hbm1"
+consumer_secret = "XszI78cpBY4ryDdHnSl1X4QCHNZ78mNFlQaXhrYkKZgw5zERDN"
 
-consumer_key = ""
-consumer_secret = ""
+access_token = "14517043-vXV9ySIfSN5Orho3DRQD6xeJ4ErRbcw0QrpIUJZz0"
+access_token_secret = "lJN8xOtbkUwUk2oBO5yA6CWM1n74EafIE1E2S7x1M6T5V"
 
-access_token = ""
-access_token_secret = ""
-
-bearer_token = ""
+bearer_token = "AAAAAAAAAAAAAAAAAAAAACLxVAEAAAAAWSP6vNtCX%2BCHnWHe4%2BJKJS%2F5Suo%3Dk8eepd7q4hAk7IHFqHDSBPqwp2EdDcnzh2l79SN6Z6KZqghZ0w"
 
 search_url = "https://api.twitter.com/2/tweets/counts/all"
+search_url2 = "https://api.twitter.com/2/tweets/search/all"
 
 
 def bearer_oauth(r):
@@ -20,7 +23,7 @@ def bearer_oauth(r):
     """
 
     r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2RecentSearchPython"
+    r.headers["User-Agent"] = "v2FullArchiveSearchPython"  # or v2RecentSearchPython for total_tweet_count().
     return r
 
 
@@ -28,10 +31,11 @@ def connect_to_endpoint(url, params):
     response = requests.get(url, auth=bearer_oauth, params=params)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
+
     return response.json()
 
 
-def main():
+def total_tweet_count():
     queries = []
     data = {"Party": ["CDU", "CSU", "SPD", "GRÜNE", "FDP", "AFD", "DIE LINKE"]}
     df = pd.DataFrame(data)
@@ -50,6 +54,47 @@ def main():
     plt.xticks(rotation=0, horizontalalignment="center")
     plt.title("Tweet count per party during preelection")
     plt.show()
+
+
+def main():
+    queries = []
+    data = {"Party": ["CDU", "CSU", "SPD", "GRÜNE", "FDP", "AFD", "DIE LINKE"]}
+
+    hashtag_list = ["#cdu", "#csu", "#spd", "#gruene", "#fdp", "#afd", "#dielinke"]
+    for hashtag in hashtag_list:
+        query = {'query': hashtag, 'start_time': '2021-07-30T00:00:00Z', 'end_time': '2021-08-01T00:00:00Z', 'max_results': '500'}
+        json_response = connect_to_endpoint(search_url2, query)
+        tweet_list = []
+        for tweet in json_response["data"]:
+            tweet_list.append((hashtag, tweet["text"]))
+        sleep(2)
+        while len(json_response["meta"]) > 3:
+            query = {'query': hashtag, 'start_time': '2021-07-31T22:00:00Z', 'end_time': '2021-08-01T00:00:00Z',
+                     'max_results': '500', 'next_token': json_response["meta"]["next_token"]}
+            json_response = connect_to_endpoint(search_url2, query)
+            if json_response["meta"]["result_count"] > 0:
+                for tweet in json_response["data"]:
+                    tweet_list.append((hashtag, tweet["text"]))
+            sleep(2)
+    df = pd.DataFrame(tweet_list, columns=['Hashtag', 'Text'])
+    pd.options.display.max_colwidth = 200
+    print(df.head())
+
+    regex_pattern = re.compile(pattern="["
+                                       u"\U0001F600-\U0001F64F"  # emoticons
+                                       u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                       u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                       "]+", flags=re.UNICODE)
+    match = ""
+    for text in df["Text"]:
+        match += re.sub(regex_pattern, '', text)  # replaces pattern with ''
+        pattern = re.compile(r'(https?://)?(www\.)?(\w+\.)?(\w+)(\.\w+)(/.+)?')
+        match = re.sub(pattern, '', text)
+        re_list = ['@[A-Za-z0–9_]+', '#']
+        combined_re = re.compile( '|'.join( re_list) )
+        match = re.sub(combined_re,'',text)
+    print(match)
 
 
 if __name__ == "__main__":
